843baa85995cd136a5476e4d152149d881146bea
Dave Fisher <dave@davefisher.tech>
2021-06-06 12:22:49 -0700

    update asfgenid plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -122,6 +122,7 @@ PLUGINS = ['asfgenid', 'pelican-gfm']
 
 # Configure the asfgenid plugin
 ASF_GENID = {
+    'unsafe_tags': False,
     'metadata': False,
     'elements': False,
     'headings': True,
